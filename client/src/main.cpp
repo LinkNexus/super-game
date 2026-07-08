d22cbac49ba82508a7bcaa0a0ed62e78cf1b5bcc
Lynce Talla <lyncejorelt@gmail.com>
2026-07-08 14:15:14 +0200

    player ship with texture added

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -1,52 +1,20 @@
 #include "raylib.h"
-#include "src/entities/Star.h"
+#include "entities/Star.h"
+#include "entities/Player.h"
 
 static constexpr int SCREEN_WIDTH = 1200;
 static constexpr int SCREEN_HEIGHT = 800;
 static constexpr int TARGET_FPS = 144;
 static constexpr float FIXED_DT = 1.0f / 60.0f;
-
-struct Player {
-  Vector2 position;
-  float rotation = 0.0f;
-  float speed = 300.0f;
-  float size = 20.f;
-
-  void update(float dt) {
-    float vec_x = 0;
-
-    if (IsKeyDown(KEY_A) || IsKeyDown(KEY_LEFT))
-      vec_x = -1.0f;
-    if (IsKeyDown(KEY_D) || IsKeyDown(KEY_RIGHT))
-      vec_x = 1.0f;
-
-    position.x += vec_x * speed * dt;
-
-    if (position.x < size)
-      position.x = size;
-    if (position.x > SCREEN_WIDTH - size)
-      position.x = SCREEN_WIDTH - size;
-    if (position.y < size)
-      position.y = size;
-    if (position.y > SCREEN_HEIGHT - size)
-      position.y = SCREEN_WIDTH - size;
-  }
-
-  void draw() {
-    Vector2 tip = {position.x, position.y - size};
-    Vector2 left = {position.x - size * 0.7f, position.y + size * 0.7f};
-    Vector2 right = {position.x + size * 0.7f, position.y + size * 0.7f};
-
-    DrawTriangle(tip, left, right, SKYBLUE);
-    DrawTriangleLines(tip, left, right, WHITE);
-  }
-};
+static constexpr int STAR_COUNT = 150;  
 
 int main(int argc, char *argv[]) {
   InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "SUPER GAME");
 
   Player player;
-  player.position = {SCREEN_HEIGHT / 2.0f, SCREEN_WIDTH / 2.0f};
+  player.position = {SCREEN_WIDTH / 2.0f, SCREEN_HEIGHT/ 2.0f};
+  player.load_texture();
+
   float accumulator = 0.0f;
 
   Star stars[STAR_COUNT];
@@ -72,6 +40,7 @@ int main(int argc, char *argv[]) {
     DrawFPS(10, 10);
     EndDrawing();
   }
+  player.unload();
 
   CloseWindow();
   return 0;
